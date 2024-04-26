2558f0fe26c52606488aef0a1bb763faa830708c
Prashant <pa99119@gmail.com>
2024-04-26 15:10:07 +0530

    added vercel.json

--- app-server/index.js
+++ app-server/index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 // mongodb
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://bookstore_demo:kYeEHBeVMiYfVnxN@cluster0.jsccyr4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
   serverApi: {
